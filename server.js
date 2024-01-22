e88f9573b4758594f159c9650d32a31ca01fcb05
Mohamedhabib-agrebi <agrebimedhbib@gmail.com>
2024-01-22 12:06:51 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+const app = express
+
+const port = 5000
+
+const testTime=(req,res,next)=>{
+    const currentDate = new Date()
+    const currentDay = currentDate.getDay()
+    const currentHour = currentDate.getHours()
+
+    if(currentDay==6 || currentDay == 0 || currentHour>= 17 || currentHour<9){
+        return res.send("Fermer")
+    }
+    next()
+}
+app.use(testTime)
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/public/Home.html')
+})
+
+app.get('/Services',(req,res)=>{
+    res.sendFile(__dirname+'/public/Services.html')
+})
+
+app.get('/Contact',(req,res)=>{
+    res.sendFile(__dirname+'/public/Contact.html')
+})
+
+
+
+
+app.listen(port,console.log ('hani hne al port $(port)'))
